c4f50d956ffdaa862ec0c29067f883346c3f5a9a
= <ziga.kokelj@gmail.com>
2020-12-08 16:33:45 +0100

    Day7 part2

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -36,12 +36,9 @@ public:
         }
         return r;
     }
-
-
-
 };
 
-int part_one(const std::vector<std::string> file_content)
+int part_one(const std::vector<std::string>& file_content)
 {
     std::vector<Bag> bags;
 
@@ -96,6 +93,70 @@ int part_one(const std::vector<std::string> file_content)
     return stack.size()-1;
 }
 
+Bag get_bag_by_name(const std::vector<Bag>& bags, const std::string& name)
+{
+    for(Bag b : bags)
+    {
+        if (b.name == name)
+        {
+            return b;
+        }
+    }
+    std::cout << "ERROR: Could not get bag: " << name << std::endl;
+    return Bag("error");
+}
+
+int part_two(const std::vector<std::string>& file_content)
+{
+    std::vector<Bag> bags;
+
+    for(std::string s : file_content)
+    {
+        std::istringstream iss(s);
+        std::vector<std::string> tokens;
+        std::copy(std::istream_iterator<std::string>(iss),
+        std::istream_iterator<std::string>(),
+        //std::ostream_iterator<std::string>(std::cout, "\n"));
+        std::back_inserter(tokens));
+
+        Bag tmp = Bag(tokens[0]+ " " + tokens[1]);
+
+        if (tokens[4] == "no")
+        {
+            bags.emplace_back(tmp);
+            continue;
+        }
+
+        for(int j=4; j < tokens.size(); j+=4)
+        {
+            BagNumberPair bnp;
+            bnp.n = std::stoi(tokens[j]);
+            bnp.name =  tokens[j+1] + " " + tokens[j+2];
+            tmp.addBagNumberPair(bnp);
+        }
+        bags.emplace_back(tmp);
+    }
+
+    std::vector<Bag> queue;
+    Bag gold= get_bag_by_name(bags, "shiny gold");
+    queue.emplace_back(gold);
+    int sum = 0;
+    
+    for(int i = 0; i < queue.size(); i++)
+    {
+        for(int j = 0; j < queue[i].content.size(); j++)
+        {
+            sum += queue[i].content[j].n;
+            for(int k = 0; k < queue[i].content[j].n; k++)
+            {
+                queue.emplace_back(get_bag_by_name(bags, queue[i].content[j].name));
+            }
+        }
+    }
+
+    return sum;
+}
+
 std::vector<std::string> read_file_line_by_line(const char* filename)
 {
     std::ifstream file{};
@@ -120,5 +181,7 @@ int main()
 
 
     std::cout << part_one(file_content) << std::endl;
+
+    std::cout << part_two(file_content) << std::endl;
 
 }
