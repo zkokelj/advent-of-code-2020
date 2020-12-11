dc223a75804ce9c3db7c0faa1f88f6f2bb207be0
= <ziga.kokelj@gmail.com>
2020-12-11 19:36:04 +0100

    Day9 part2

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <tuple>
 
+unsigned long long sum_list(const std::vector<unsigned long long>& numbers, int start, int end)
+{
+    unsigned long long sum = 0;
+    for(int i = start; i < end; i++)
+    {
+        sum += numbers[i];
+    }
+    return sum;
+}
 
-unsigned long long part_one(const std::vector<unsigned long long>& numbers, int preamble)
+std::tuple<unsigned long long, unsigned long long> return_min_max(const std::vector<unsigned long long>& numbers, int start, int end)
+{
+    unsigned long long min = numbers[start];
+    unsigned long long max = numbers[start];
+
+    for(int i = start; i < end; i++)
+    {
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }else if(numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    }
+    return std::make_tuple(min, max);
+}
+
+std::tuple<unsigned long long, int> part_one(const std::vector<unsigned long long>& numbers, int preamble)
 {
     for(int i =preamble; i < numbers.size(); i++)
     {
@@ -27,13 +55,38 @@ unsigned long long part_one(const std::vector<unsigned long long>& numbers, int 
         }
         if (!found)
         {
-            return number;
+            return std::make_tuple(number, i);
+        }
+    }
+
+    return std::make_tuple(0, 0);
+}
+
+unsigned long long part_two(const std::vector<unsigned long long>& numbers, int index)
+{
+    unsigned long long n = numbers[index];
+
+    unsigned long long min;
+    unsigned long long max;
+
+    for(int i = 0; i < index; i++)
+    {
+        for(int j = i+1; j < index; j++)
+        {
+            unsigned long long sum = sum_list(numbers, i, j);
+            if (sum == n)
+            {
+                std::tie(min, max) = return_min_max(numbers, i, j);
+                return min+max;
+            }else if(sum > n)
+            {
+                break;
+            }
         }
     }
 
     return 0;
 }
-
 
 
 int main()
@@ -50,6 +103,18 @@ int main()
 	}
     in.close(); // close file stream
 
-    std::cout << "Part one: " << part_one(numbers, 25) << std::endl;
+    unsigned long long n;
+    int index;
+    std::tie(n, index) = part_one(numbers, 25);
 
+    std::cout << "Part one: " << n << " at inde: " << index << std::endl;
+
+    //std::cout << "Sum: " << sum_list(numbers, 0, 3) << std::endl;
+    unsigned long long min;
+    unsigned long long max;
+    std::tie(min, max) = return_min_max(numbers, 0, 3);
+
+    //std::cout << min << " and " << max << std::endl;
+
+    std::cout << "Part two: " << part_two(numbers, index);
 }
