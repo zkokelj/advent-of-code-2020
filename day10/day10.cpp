394fb69cdb1f1b9241c70848c47727b78cc77855
= <ziga.kokelj@gmail.com>
2020-12-14 06:03:59 +0100

    day10 part2

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <unordered_map>
 
 int part_one(const std::vector<int>& vec)
 {
@@ -34,6 +35,48 @@ int part_one(const std::vector<int>& vec)
     return adapters1 * adapters3;
 }
 
+uint64_t part_two(std::vector<int>& vec)
+{   
+    vec.push_back(vec[vec.size()-1]+3);
+    std::unordered_map<int, uint64_t> memo = { {0, 1} };
+
+    std::unordered_map<int, uint64_t>::iterator it;
+    int largest_index = 0;
+
+    for (int x : vec)
+    {
+        uint64_t part1 = 0;
+        it = memo.find(x-3);
+        if (it != memo.end())
+        {
+            part1 = it->second;
+        }
+
+        uint64_t part2 = 0;
+        it = memo.find(x-2);
+        if (it != memo.end())
+        {
+            part2 = it->second;
+        }
+
+        uint64_t part3 = 0;
+        it = memo.find(x-1);
+        if (it != memo.end())
+        {
+            part3 = it->second;
+        }
+
+        memo.insert({x, part1+part2+part3});
+        largest_index = x;
+    }
+    it = memo.find(largest_index);
+    if (it != memo.end())
+    {
+        return it->second;
+    }
+    // this should never happen
+    return -1;
+}
 
 int main()
 {
@@ -52,4 +95,6 @@ int main()
     std::sort(numbers.begin(), numbers.end()); 
 
     std::cout << "Part1: " << part_one(numbers) << std::endl;
+
+    std::cout << "Part2: " << part_two(numbers) << std::endl;
 }
